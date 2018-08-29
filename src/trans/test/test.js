bd817ce4c92f77dfec23acad0773acb57e6ca953
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2018-08-29 18:46:07 +0800

    initial commit

--- src/trans/test/test.js
+++ src/trans/test/test.js
@@ -18,30 +18,37 @@ let config = {
 let input = {
 
 };
-
-// let ccE20Approve = new CrossChinE20Approve(input,config);
-// ccE20Approve.run();
-
+console.log("==================================");
+console.log("============BTC Lock============");
 (new CrossChainBtcLock(input,config)).run();
-console.log("========================");
+console.log("==================================");
+console.log("============BTC Refund============");
 (new CrossChainBtcRefund(input,config)).run();
-console.log("========================");
+console.log("==================================");
+console.log("============BTC Revoke============");
 (new CrossChainBtcRevoke(input,config)).run();
-console.log("========================");
+console.log("==================================");
+console.log("============ETH Lock============");
 (new CrossChainEthLock(input,config)).run();
-console.log("========================");
+console.log("==================================");
+console.log("============ETH Refund============");
 (new CrossChainEthRefund(input,config)).run();
-console.log("========================");
+console.log("==================================");
+console.log("============ETH Revoke============");
 (new CrossChainEthRevoke(input,config)).run();
-console.log("========================");
+console.log("==================================");
+console.log("============E20 Approve============");
 (new CrossChainE20Approve(input,config)).run();
-console.log("========================");
+console.log("==================================");
+console.log("============E20 Lock============");
 (new CrossChainE20Lock(input,config)).run();
-console.log("========================");
+console.log("==================================");
+console.log("============E20 Revoke============");
 (new CrossChainE20Revoke(input,config)).run();
-console.log("========================");
+console.log("==================================");
+console.log("============E20 Refund============");
 (new CrossChainE20Refund(input,config)).run();
-console.log("========================");
+console.log("==================================");
 
 
 

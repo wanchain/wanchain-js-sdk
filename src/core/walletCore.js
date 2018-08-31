571cf178789caa9db046a6eeeadc732833b09d7b
Jason He <hejian_best@163.com>
2018-08-31 11:12:27 +0800

    add socket&sender to global

--- src/core/walletCore.js
+++ src/core/walletCore.js
@@ -1,36 +1,42 @@
 "use strict";
 
-const wanchainTrans = require("./wanchainTrans");
-const SendTransaction = require('./crossSend/sendTransaction');
-const { Keystore } = require('./keystore');
-const { SendFromSocket, SendFromWeb3, SocketServer} = require('./sender');
-
+const util = require('../api/util');
+const trans = require('../trans');
+const baseCfg = require('../conf/config');
+const { KeystoreDir } = require('../keystore');
+const { SendFromSocket, SendFromWeb3, SocketServer} = require('../sender');
 
 class WalletCore {
     constructor(config, chain, logger){
+        global.config = this.config = Object.assign(baseCfg, config);
         global.getLogger = logger;
         this.chain = chain;
-        this.config = config;
-        this.wanchainTrans = wanchainTrans;
-        this.crossKeystoreDir = global.crossKeystoreDir = new Keystore(config.crossKeystorePath);
-        this.originalKeystoreDir =  global.originalKeystoreDir = new Keystore(config.originalKeystorePath);
+        this.trans = trans;
+        this.util = util;
+        this.init();
     }
  
-    init() {
-        const socket = new SocketServer(this.config.socketUrl);
+    async init() {
+        this.config.crosschainDir = new KeystoreDir(this.config.crosschainPath);
+        this.config.wanDir = new KeystoreDir(this.config.WanPath);
 
-        this.crossSend = new SendFromSocket(socket, this.chain['cross']);
-        this.originalSend = new SendFromSocket(socket, this.chain['original']);
-        global.crossSend = this.crossSend;
-        global.originalSend = this.originalSend;
-        return new Promise((resolve, reject) => {
-            socket.on('error', (err) => {
-                reject(err);
-            });
-            socket.on('open', () => {
-                resolve('success')
-            })
+        this.socket = global.socket = new SocketServer(this.config.socketUrl);
+        this.sender = global.sender = new sendFromSocket(this.socket, this.chain);
+
+        socket.connection.on('error', (err) => {
+            this.socket = global.socket = new SocketServer(this.config.socketUrl);
         });
+        socket.connection.on('open', () => {
+            if (this.config.useLocalNode && !this.web3Sender) {
+                this.web3Sender = this.createWeb3Sender(this.config.rpcIpcPath);
+            }
+        });
+        try {
+            [this.config.lockedTime, this.config.c2wRatio] = await Promise.all([util.getEthLockTime(this.sender), util.getEthC2wRatio(this.sender)]);            
+        } catch(err) {
+            console.log(err);
+        }
+        this.db = global.db = util.initDb(this.config, sender);
     }
 
     createSendTransaction(chainType) {

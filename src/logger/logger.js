49638aa6a93c6799813a51363d8c14f7ff6e7c18
Jason He <hejian_best@163.com>
2018-09-05 18:22:37 +0800

    add logger func

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,78 +1,56 @@
-const winston = require("winston");
-const moment = require('moment');
 const util = require('util');
-
-require('winston-syslog').Syslog;
-require('winston-daily-rotate-file');
-
-
-const MESSAGE = Symbol.for('message');
-const SPLAT = Symbol.for('splat');
+const path = require('path');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
 
 
-/**
- * logger support 4 level
- * @info
- * @debug
- * @warn
- * @error
- */
+const comFormat = printf(info => {
+    return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
+});
+
+const debugFormat = printf(info => {
+    return `${util.inspect(info.message)}`;
+});
+
+const defaultPath = path.join(__dirname, '../../', 'log');
 
 class Logger {
-    constructor(name, file, errorFile, level = 'info') {
-        this.logger = winston.createLogger({
-            levels: winston.config.syslog.levels,
-            level: level,
-            format: winston.format(function (info, opts) {
-                let prefix = util.format('%s %s %s %s', "walletCli", moment().format('YYYY-MM-DD hh:mm:ss,SSS').trim(), name, info.level.toUpperCase());
-                if (info[SPLAT]) {
-                    info[MESSAGE] = util.format('%s %s', prefix, util.format(info.message, ...info[SPLAT]));
-                } else {
-                    info[MESSAGE] = util.format('%s %s', prefix, util.format(info.message));
-                }
-                return info;
-            })(),
+    constructor(logPath = defaultPath) {
+        this.logger = createLogger({
+            level: 'info',
+            format: combine(
+                label({
+                    label: 'wanSdk'
+                }),
+                timestamp(),
+                comFormat
+            ),
             transports: [
-                //
-                // - Write to all logs with level `level` and below to file
-                // - Write all logs error (and below) to errorFile.
-                //
-                // new winston.transports.Syslog(this.options),
-                new winston.transports.Console(),
-                // new (winston.transports.DailyRotateFile)({
-                //   filename: errorFile,
-                //   level: 'error',
-                //   datePattern: 'YYYY-MM-DD-HH',
-                //   zippedArchive: false,
-                //   maxSize: '20m',
-                //   maxFiles: '5d'
-                // }),
-                new(winston.transports.DailyRotateFile)({
-                    filename: file,
-                    level: level,
-                    datePattern: 'YYYY-MM-DD',
-                    zippedArchive: false,
-                    maxSize: '50m',
-                    maxFiles: '5d'
+                new transports.File({
+                    filename: `${logPath}/combined.log`
+                }),
+                new transports.File({
+                    filename: `${logPath}/error.log`,
+                    level: 'error'
+                })
+            ],
+            exceptionHandlers: [
+                new transports.File({
+                    filename: `${logPath}/exceptions.log`
                 })
             ]
         });
+
+        this.init();
     }
 
-    debug(...params) {
-        this.logger.debug(...params);
-    }
-
-    info(...params) {
-        this.logger.info(...params);
-    }
-
-    warn(...params) {
-        this.logger.warning(...params);
-    }
-
-    error(...params) {
-        this.logger.error(...params);
+    init() {
+        if (process.env.NODE_ENV !== 'production') {
+            this.logger.add(new transports.Console({
+                format: debugFormat,
+                level: 'debug'
+            }));
+        }
     }
 }
 

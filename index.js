4ca84461f92af18af5a26337521c913dda408132
ktfc76 <ktfc76@hotmail.com>
2019-03-15 14:54:51 +0800

    Export hd util

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 exports.walletCore = require('./src/core/walletCore');
 exports.ccUtil = require('./src/api/ccUtil');
+exports.hdUtil = require('./src/api/hdUtil');
 exports.btcUtil = require('./src/api/btcUtil');
 exports.upgrade = require('./src/db/upgrade/upgrade');
 exports.CrossChain = require('./src/trans/cross-chain');

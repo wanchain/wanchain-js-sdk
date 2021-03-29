2ccaf3f7519e3f93102d32518e950fc79d35e720
lansehuiyi6 <lansehuiyi6@163.com>
2021-03-29 11:19:43 +0800

    fix eos locked time mainnet bug

--- src/core/walletCore.js
+++ src/core/walletCore.js
@@ -444,13 +444,15 @@ class WalletCore extends EventEmitter {
       global.lockedTime = '3600';
       global.lockedTimeBTC = '14400';
       global.btc2WanRatio  = '10000';
-      global.lockedTimeEOS = '3600';
-
+      
       utils.setConfigSetting("wanchain:crosschain:locktime", global.lockedTime);
       utils.setConfigSetting("wanchain:crosschain:btclocktime", global.lockedTimeBTC);
       utils.setConfigSetting("wanchain:crosschain:bt2wanRatio", global.btc2WanRatio);
 
       global.nonceTest = 0x0;          // only for test.
+
+      let eosLoedTime = await ccUtil.getEosLockTime();
+      global.lockedTimeEOS = eosLoedTime[3];
 
       let eosChainInfo = await ccUtil.getEosChainInfo();
       global.eosChainId = eosChainInfo.chain_id;
@@ -465,6 +467,7 @@ class WalletCore extends EventEmitter {
       logger.info("initGlobalScVar is completed");
       return true;
     } catch (err) {
+      global.lockedTimeEOS = (this.config.network === 'testnet') ? '3600' : '129600';
       global.eosChainId = (this.config.network === 'testnet') ? '2a02a0053e5a8cf73a56ba0fda11e4d92e0238a4a2aa74fccf46d5a910746840' : 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906';
       logger.error("Caught error in initGlobalScVar: ", err);
       return true;
